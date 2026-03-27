eaa9e8084b490c25619cf73296ad41b78b4ef95f
Vojtěch Peňák <penakvo22@sps-prosek.lad>
2026-03-27 09:29:40 +0100

    MainScene aDDDED GUYS

--- Scenes/MainScene/main_screen.gd
+++ Scenes/MainScene/main_screen.gd
@@ -2,24 +2,33 @@ extends Control
 
 @onready var VBOXc = $Panel/VBoxContainer
 @onready var selections = []
-var currecnt_selection = ""
+var selection_index = 0
+var current_selection = ""
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	selections = VBOXc.get_children().lowercase()
+	selections = VBOXc.get_children()
 	
 	pass # Replace with function body.
 
 
 func _input(event: InputEvent) -> void:
 	if Input.is_action_just_pressed("interact"):
-		_get_game_scene(currecnt_selection)
+		_get_game_scene(current_selection)
+	
+	if Input.is_action_just_pressed("ui_down"):
+		selection_index + 1
+		if selection_index > selections.len():
+			selection_index = 0
+		current_selection = [selection_index]
 		
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+	if Input.is_action_just_pressed("ui_up"):
+		selection_index - 1 
+		current_selection = [selection_index]
+		
 func _process(delta: float) -> void:
 	pass
 
 func _get_game_scene(selected): 
 	if selected in selections:
-		pass
+		selections[selected]
